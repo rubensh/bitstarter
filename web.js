5d4e0c6ee8c726fcafa038b9807128e98236a12c
Ruben Santana Hernandez <rubensh1980@gmail.com>
2013-07-21 14:05:42 +0000

    Updated web.js to listen on port 8080.

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
   response.send(welcomeString);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+var port = process.env.PORT || 8080;
+  app.listen(port, function() {
   console.log("Listening on " + port);
 });
